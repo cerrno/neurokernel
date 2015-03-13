ac464a975a2831365c371c9ae0bd51a34845b9df
Lucas Schuermann <lucas.v.schuermann@gmail.com>
2015-03-13 17:56:27 -0400

    Initial commit of work on Morris-Lecar RK4 and HH implementation

---
+++ examples/testLPU/testLPU_exec.py
@@ -0,0 +1,28 @@
+#@author: Amol Kapoor
+#@date: 3-13-15
+#Executor of simple LPU defined in data
+
+from neurokernel.core import Manager
+from neurokernel.LPU.LPU import LPU
+from neurokernel.tools.comm import get_random_port
+
+dt = 1e-4
+dur = 1.0
+Nt = int(dur/dt)
+
+port_data = get_random_port()
+port_ctrl = get_random_port()
+
+(n_dict, s_dict) = LPU.lpu_parser('./data/simple_lpu.gexf.gz')
+
+lpu = LPU(dt, n_dict, s_dict, input_file='./data/simple_input.h5', output_file = 'simple_output.h5', port_ctrl=port_ctrl, port_data=port_data, device = 0, id='simple', debug = False)
+
+man = Manager(port_data, port_ctrl)
+
+man.add_brok()
+
+man.add_mod(lpu)
+
+man.start(steps=Nt)
+
+man.stop()
