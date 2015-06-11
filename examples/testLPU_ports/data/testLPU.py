5e290ddd0b31c18dd45c6df5372a256a5915674b
amol <amol@huxley.ee.columbia.edu>
2015-06-10 20:56:53 -0400

    Added testLPU_ports, fake neuron

---
+++ examples/testLPU_ports/data/testLPU.py
@@ -0,0 +1,170 @@
+#An implementation of a 2 neuron lpu that instantiates a Leaky and a Morrislecar 
+#into a 1 edge/2node graph. Outputs to a .gefx file. Take from py notebooks. 
+
+#matrix support
+import numpy as np
+np.set_printoptions(threshold = 'nan')
+#graph support
+import networkx as nx
+
+#input support
+import h5py
+
+def create_lpu_0():
+    #sets up neurons/networks
+    G = nx.DiGraph()
+
+        #sets up node connections in graph
+    G.add_nodes_from([0,1])
+
+    G.node[0] = {
+            'name': 'neuron_0',
+            'selector':'/a[0]', 
+            'model': 'MorrisLecar_a',
+            'extern': True, 
+            'public': True, 
+            'spiking': False, 
+            'V1': -1.2,
+            'V2': 18.0,
+            'V3': 2.0,
+            'V4': 30.0,
+            'V_l': -60.0, 
+            'V_ca': 120.0, 
+            'V_k': -84.0, 
+            'G_l': 2.0, 
+            'G_ca': 4.0, 
+            'G_k': 8.0,
+            'phi': 0.04, 
+            'offset': 0.0,
+            'initV': -50.0,
+            'initn': 0.03       
+        }
+
+
+    G.node[1] = {
+            'name': 'neuron_1',
+            'selector':'/lpu_0/out/gpot/0', 
+            'model': 'MorrisLecar_a',
+            'extern': False, 
+            'public': True, 
+            'spiking': False, 
+            'V1': -1.2,
+            'V2': 18.0,
+            'V3': 2.0,
+            'V4': 30.0,
+            'V_l': -60.0, 
+            'V_ca': 120.0, 
+            'V_k': -84.0, 
+            'G_l': 2.0, 
+            'G_ca': 4.0, 
+            'G_k': 8.0,
+            'phi': 0.04, 
+            'offset': 0.0,
+            'initV': -50.0,
+            'initn': 0.03       
+        }
+
+    #From input to output
+    G.add_edge(0, 1, type='directed', attr_dict={
+        'name': G.node[0]['name']+'-'+G.node[1]['name'],
+        'model'       : 'power_gpot_gpot_sig',
+        'class'       : 3,
+        'slope'       : 0.8,
+        'threshold'   : -45,
+        'power'       : 10.0,
+        'saturation'  : 30.0,
+        'delay'       : 1.0,
+        'reverse'     : -0.08,
+        'conductance' : True})
+
+    nx.write_gexf(G, 'simple_lpu_0.gexf.gz')
+
+
+
+
+def create_lpu_1():
+    #sets up neurons/networks
+    G = nx.DiGraph()
+
+    #sets up node connections in graph
+    G.add_nodes_from([0,1])
+
+    G.node[0] = {
+            'name': 'port_in_gpot_0',
+            'model': 'port_in_gpot',
+            'selector': '/lpu_1/in/gpot/0', 
+            'spiking': False,
+            'public': True,
+            'extern': False
+            }
+
+    #MorrisLecar updated
+    G.node[1] = {
+            'model': 'MorrisLecar_a',
+            'name': 'neuron_1',
+            'extern': False, 
+            'public': True, 
+            'spiking': False, 
+            'selector':'/a[2]', 
+            'V1': -1.2,
+            'V2': 18.0,
+            'V3': 2.0,
+            'V4': 30.0,
+            'V_l': -60.0, 
+            'V_ca': 120.0, 
+            'V_k': -84.0, 
+            'G_l': 2.0, 
+            'G_ca': 4.0, 
+            'G_k': 8.0,
+            'phi': 0.04, 
+            'offset': 0.0,
+            'initV': -50.0,
+            'initn': 0.03       
+        }
+
+        
+    #From input port to output
+    G.add_edge(0, 1, type='directed', attr_dict={
+        'name': G.node[0]['name']+'-'+G.node[1]['name'],
+        'model'       : 'power_gpot_gpot_sig',
+        'class'       : 3,
+        'slope'       : 0.8,
+        'threshold'   : -45,
+        'power'       : 10.0,
+        'saturation'  : 30.0,
+        'delay'       : 1.0,
+        'reverse'     : -0.08,
+        'conductance' : True})
+
+
+
+    nx.write_gexf(G, 'simple_lpu_1.gexf.gz')
+
+
+#sets up input file
+
+#time step
+dt = 1e-4
+#duration
+dur = 1.0
+#number of datapoints
+Nt = int(dur/dt)
+
+#start and stop of input
+start = 0.3
+stop = 0.6
+
+#the current input
+I_max = 6
+t = np.arange(0, dt*Nt, dt)
+I = np.zeros((Nt, 1), dtype=np.double)
+
+#inputs current at points indicated
+I[np.logical_and(t > start, t < stop)] = I_max
+
+with h5py.File('simple_input.h5', 'w') as f: 
+    f.create_dataset('array', (Nt, 1), dtype = np.double, data = I)
+
+
+create_lpu_0()
+create_lpu_1()
---
+++ examples/testLPU_ports/visualize_testLPU.py
@@ -0,0 +1,73 @@
+#@author: Amol Kapoor
+#date: 3-13-15
+#Visualizer for simpleLPU stuff
+
+import matplotlib as mpl
+mpl.use('agg')
+
+import neurokernel.LPU.utils.visualizer as vis
+import networkx as nx
+
+# Temporary fix for bug in networkx 1.8:
+nx.readwrite.gexf.GEXF.convert_bool = {'false':False, 'False':False,
+                                       'true':True, 'True':True}
+#starts up the visualizer code
+V = vis.visualizer()
+
+#takes in the input file as defined in data, and plots it
+V.add_LPU('./data/simple_input.h5', LPU='Input')
+V.add_plot({'type': 'waveform', 'ids': [[0]]}, 'input_Input')
+
+#takes in the spike data/potential from the neuron output and plots it
+V.add_LPU('simple_output_spike.h5',
+          './data/simple_lpu.gexf.gz', 'Simple LPU (Spikes)')
+
+#the [0,1] under ids should print both hh and leaky
+V.add_plot({'type':'raster', 'ids': {0: [0]},
+            'yticks': [0], 'yticklabels': [0]},
+            'Simple LPU (Spikes)','Output')
+
+V.add_LPU('simple_output_gpot.h5',
+          './data/simple_lpu.gexf.gz', 'Simple LPU (Graded Potential)')
+
+V.add_plot({'type': 'waveform', 'ids': {0:[0]}},
+            'Simple LPU (Graded Potential)', 'Output')
+
+V.add_plot({'type': 'waveform', 'ids': {0:[1]}},
+            'Simple LPU (Graded Potential)', 'Output')
+
+V.add_plot({'type': 'waveform', 'ids': {0:[2]}},
+            'Simple LPU (Graded Potential)', 'Output')
+
+
+
+
+#vars for plots
+
+#how often it updates
+V._update_interval = 50
+
+#rows and colums to plot in terms of size
+V.rows = 5
+V.cols = 1 
+
+#self explantory
+V.fontsize = 10
+#V.out_filename = 'simple_output.avi'
+#V.codec = 'libtheora'
+
+#time step
+V.dt = 0.0001
+
+#Changes the sizes on the axis
+V.xlim = [0, 1.0]
+V.ylim = [-70.0, 10.0]
+
+#figure size
+V.figsize = (16, 9)
+
+V.title = "Simple LPU Testing RK4 Models"
+
+#runs the visualizer
+V.run('simple_output.png', 120)
+
