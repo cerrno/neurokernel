9c484d7c896f59eca7b8a4b670dc09e3162dc878
amol <amol@huxley.ee.columbia.edu>
2015-08-11 18:36:29 -0400

    Updated input for prebaked info

--- examples/testLPU_io/io_realtime_input.py
+++ examples/testLPU_io/io_realtime_input.py
@@ -25,6 +25,12 @@ def main(data_size):
     #How often to send data (slightly over 30 frames per second)
     cycle_time = .034
 
+    data_array = []
+
+    for i in xrange(0, 3500):
+        data = [round(random.random()*100 - 50) for _ in xrange(data_size/3)]
+        data_array.append(json.dumps(data) + "_")
+        print len(data_array)
 
     #socket connection details
     host = 'localhost' 
@@ -38,19 +44,14 @@ def main(data_size):
     while 1: 
         start_time = current_milli_time()
 
-        data = [random.random()*100 - 50 for _ in xrange(data_size)]
-
-        start_time = current_milli_time()
-
-        data_str = json.dumps(data)
-
-        data_str = data_str + "_"
-
-        s.send(data_str) 
+        print (len(data_array[count])*2)
+        s.send(data_array[count]) 
 
         print current_milli_time() - start_time
 
         time.sleep(max(0, cycle_time - start_time))
+
+        count = count+1
 
 if __name__ == "__main__":
     main(ARRAYSIZE)
